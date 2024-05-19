a346a65ff93568ae41c4aaa724c7a7454255d0a5
Joel S. <57820144+joelspa@users.noreply.github.com>
2024-05-19 02:32:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,47 +1,21 @@
 // create a web server
+// create a route for comments
+// return a list of comments
+
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var qs = require('querystring');
 
-// Load the comments from the file
-var comments = require('./comments.json');
-
-// Function to handle POST requests
-function addComment(req, res) {
-  var body = '';
-  req.on('data', function(data) {
-    body += data;
-  });
-  req.on('end', function() {
-    var comment = qs.parse(body);
-    comments.push(comment);
-    fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
-      if (err) {
-        console.error(err);
-        res.end('Server error');
-      }
-      res.end('Success');
-    });
-  });
-}
-
-// Function to handle GET requests
-function getComments(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-    'Cache-Control': 'no-cache',
-    'Access-Control-Allow-Origin': '*'
-  });
-  res.end(JSON.stringify(comments));
-}
-
-// Create the server
-http.createServer(function(req, res) {
-    if (req.method === 'POST') {
-        return addComment(req, res);
+http.createServer(function(request, response) {
+    var url_parts = url.parse(request.url);
+    if(url_parts.pathname === '/comments') {
+        fs.readFile('comments.json', function(err, data) {
+            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.end(data);
+        });
+    } else {
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.end('Page not found');
     }
-    if (req.method === 'GET') {
-        return getComments(req, res);
-    }
-}).listen(3000, 'localhost'); // Provide a valid value for the second argument
+}).listen(3000);
+console.log('Server listening on port 3000');
